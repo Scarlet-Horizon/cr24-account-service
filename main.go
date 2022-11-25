a964cffb02823fcac1bc1f7e261f3d03c18a5571
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-11-25 14:57:06 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"log"
+	"main/db"
 	"main/env"
+	"main/util"
+	"math/rand"
+	"os"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	err := env.Load("env/.env")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to load env variables: %v", err)
 	}
+
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(os.Getenv("REGION")))
+	if err != nil {
+		log.Fatalf("failed to load SDK config, %v", err)
+	}
+
+	acc := db.AccountDB{
+		Client: dynamodb.NewFromConfig(cfg),
+	}
+
+	err = acc.Create(util.RandomAccount())
+	fmt.Println(err)
 }
