4e0620d9ecf31b4057385a5c52376e39ab65e731
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-11-25 14:54:54 +0100

    Add account model

--- model/account.go
+++ model/account.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Account struct {
-	PK            string    `dynamodbav:"PK"`
-	SK            string    `dynamodbav:"SK"`
-	AccountNumber int       `dynamodbav:"AccountNumber"`
-	Amount        float64   `dynamodbav:"Amount"`
-	Limit         float64   `dynamodbav:"Limit"`
-	OpenDate      time.Time `dynamodbav:"OpenDate"`
-	CloseDate     time.Time `dynamodbav:"CloseDate"`
-	Type          string    `dynamodbav:"Type"`
+	PK string `dynamodbav:"PK"`
+	SK string `dynamodbav:"SK"`
+	//AccountNumber int       `dynamodbav:"AccountNumber"`
+	Amount    float64   `dynamodbav:"Amount"`
+	Limit     int       `dynamodbav:"Limit"`
+	OpenDate  time.Time `dynamodbav:"OpenDate"`
+	CloseDate time.Time `dynamodbav:"CloseDate,omitempty"`
+	Type      string    `dynamodbav:"Type"`
 }
