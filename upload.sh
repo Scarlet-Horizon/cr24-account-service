cc07aa5decf768669693e1cb6ab025d7f6adb363
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-12-01 09:26:52 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,15 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 
-docker build --tag davidslatinek/account-api:0.1 .
-docker tag davidslatinek/account-api:0.1 davidslatinek/account-api:0.1
-docker push davidslatinek/account-api:0.1
+if [ $# -eq 0 ]; then
+  echo "Specify tag number!"
+  exit 1
+fi
+
+if ! [[ $1 =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then
+  echo "Argument must be a number"
+  exit 1
+fi
+
+docker build --tag davidslatinek/account-api:"$1" .
+docker tag davidslatinek/account-api:"$1" davidslatinek/account-api:"$1"
+docker push davidslatinek/account-api:"$1"
