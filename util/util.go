e00529d82f36216ab3946ab2c938584475dc8982
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-12-06 17:02:03 +0100

    Update CORS

--- util/util.go
+++ util/util.go
@@ -164,5 +164,26 @@ func CORS(context *gin.Context) {
 	context.Header("Access-Control-Allow-Origin", "*")
 	context.Header("Access-Control-Allow-Headers", "*")
 	context.Header("Access-Control-Allow-Methods", "*")
+
+	if context.Request.Method == "OPTIONS" {
+		context.AbortWithStatus(200)
+		return
+	}
 	context.Next()
 }
+
+//func CORSMiddleware() gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+//		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+//		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+//		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PATCH, DELETE, PUT")
+//
+//		if c.Request.Method == "OPTIONS" {
+//			c.AbortWithStatus(204)
+//			return
+//		}
+//
+//		c.Next()
+//	}
+//}
