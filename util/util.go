cbaa0ac73944f6757e7993c905b45a1d26c0bfc6
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-12-06 18:01:37 +0100

    Update CORS

--- util/util.go
+++ util/util.go
@@ -162,28 +162,14 @@ func ValidateToken(context *gin.Context) {
 
 func CORS(context *gin.Context) {
 	context.Header("Access-Control-Allow-Origin", "*")
+	context.Header("Access-Control-Allow-Credentials", "true")
 	context.Header("Access-Control-Allow-Headers", "*")
 	context.Header("Access-Control-Allow-Methods", "*")
+	context.Header("Access-Control-Max-Age", "86400")
 
-	if context.Request.Method == "OPTIONS" {
-		context.AbortWithStatus(200)
+	if context.Request.Method == http.MethodOptions {
+		context.AbortWithStatus(http.StatusOK)
 		return
 	}
 	context.Next()
 }
-
-//func CORSMiddleware() gin.HandlerFunc {
-//	return func(c *gin.Context) {
-//		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-//		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-//		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-//		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PATCH, DELETE, PUT")
-//
-//		if c.Request.Method == "OPTIONS" {
-//			c.AbortWithStatus(204)
-//			return
-//		}
-//
-//		c.Next()
-//	}
-//}
